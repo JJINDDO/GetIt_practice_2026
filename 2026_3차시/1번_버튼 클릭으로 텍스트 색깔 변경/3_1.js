ef610af3773788600d017eac763fbb711dca1a1c
DohyunJin <jindohyun2006@gmail.com>
2026-04-30 19:20:51 +0900

    Revert "assignment add"
    This reverts commit 4edcf302c1cbbdf593ebec7a613fc80c82a75e8c.

--- 2026_3차시/1번_버튼 클릭으로 텍스트 색깔 변경/3_1.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('colorBtn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click', () => {
-  text.style.color = 'red';
-});
--- 2026_3차시/3번_버튼 클릭으로 숫자 증가/3_3.js
+++
@@ -1,8 +0,0 @@
-const countBtn = document.getElementById('countBtn');
-const countDisplay = document.getElementById('countDisplay');
-let count = 0;
-
-countBtn.addEventListener('click', () => {
-  count++;
-  countDisplay.textContent = count; // span한 부분의 텍스트를 count 변수에 든 값으로 수정
-});
